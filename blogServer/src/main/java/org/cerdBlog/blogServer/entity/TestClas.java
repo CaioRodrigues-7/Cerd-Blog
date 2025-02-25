1ccd21921d6c08bdc43f2963896713255a2fd7f8
Cerd7 <cerd7@proton.me>
2025-02-24 23:45:28 -0300

    Start of my blog w/ springboot - 0.1

---
+++ blogServer/src/main/java/org/cerdBlog/blogServer/entity/TestClas.java
@@ -0,0 +1,11 @@
+package org.cerdBlog.blogServer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class TestClas {
+
+    @Id
+    private Long id;
+}
